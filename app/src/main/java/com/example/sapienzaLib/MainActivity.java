36447804400dc2c8a468d666171110747dc33058
soapwaster <colitta.valerio@gmail.com>
2019-02-15 17:13:02 +0100

    add sign in intent after

--- app/src/main/java/com/example/sapienzaLib/MainActivity.java
+++ app/src/main/java/com/example/sapienzaLib/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity
         Intent intent = getIntent();
         intent.getStringExtra("user_name");
         intent.getStringExtra("user_email");
-        
+
 
     }
 
