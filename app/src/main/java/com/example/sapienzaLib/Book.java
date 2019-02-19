ace6eff2eb04fd45e7443b0dd0cb5451d19096e1
soapwaster <colitta.valerio@gmail.com>
2019-02-19 09:40:17 +0100

    add book recicleview

---
+++ app/src/main/java/com/example/sapienzaLib/Book.java
@@ -0,0 +1,44 @@
+package com.example.sapienzaLib;
+
+public class Book {
+    private String title,author,description,thumbnail;
+
+    public Book(String title, String author, String description, String thumbnail) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
--- app/src/main/java/com/example/sapienzaLib/SearchFragment.java
+++ app/src/main/java/com/example/sapienzaLib/SearchFragment.java
@@ -4,6 +4,8 @@ package com.example.sapienzaLib;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,9 @@ import java.util.List;
  */
 public class SearchFragment extends Fragment {
 
+    private RecyclerView lw;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -34,41 +39,18 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        lw = (RecyclerView) rootView.findViewById(R.id.list_search);
 
-        //Create the Person objects
-        Booking book1 = new Booking("title1","author1");
-        Booking book2 = new Booking("title2","author1");
-        Booking book3 = new Booking("title3","author1");
-        Booking book4 = new Booking("title4","author1");
-        Booking book5 = new Booking("title5","author1");
-        Booking book6 = new Booking("title5","author1");
-        Booking book7 = new Booking("title5","author1");
-        Booking book8 = new Booking("title5","author1");
-        Booking book9 = new Booking("title5","author1");
-        Booking book10 = new Booking("title5","author1");
-        Booking book11= new Booking("title5","author1");
+        ArrayList<Book> mBooks = new ArrayList<Book>();
 
+        mBooks.add(new Book("Moby tumadre", "Anjelocat", "Sceinadeimerda", "/Users/salvatorecolitta/AndroidStudioProjects/SapienzaLib/app/src/main/res/drawable/logo_sap.png"));
+        mBooks.add(new Book("Moby tumadre", "Anjelocat", "Sceinadeimerda", "/Users/salvatorecolitta/AndroidStudioProjects/SapienzaLib/app/src/main/res/drawable/logo_sap.png"));
+        mBooks.add(new Book("Moby tumadre", "Anjelocat", "Sceinadeimerda", "/Users/salvatorecolitta/AndroidStudioProjects/SapienzaLib/app/src/main/res/drawable/logo_sap.png"));
+        mBooks.add(new Book("Moby tumadre", "Anjelocat", "Sceinadeimerda", "/Users/salvatorecolitta/AndroidStudioProjects/SapienzaLib/app/src/main/res/drawable/logo_sap.png"));
 
-        //Add the Person objects to an ArrayList
-        ArrayList<Booking> bookingList = new ArrayList<>();
-        bookingList.add(book1);
-        bookingList.add(book2);
-        bookingList.add(book3);
-        bookingList.add(book4);
-        bookingList.add(book5);
-        bookingList.add(book6);
-        bookingList.add(book7);
-        bookingList.add(book8);
-        bookingList.add(book9);
-        bookingList.add(book10);
-        bookingList.add(book11);
-
-        BookingListAdapter adapter = new BookingListAdapter(getActivity().getBaseContext(), R.layout.booking_view_layout, bookingList);
-        ListView lw = (ListView) rootView.findViewById(R.id.list_search);
-        lw.setAdapter(adapter);
-
-        lw.setDividerHeight(0);
-        lw.setDivider(null);
+        mAdapter = new BookListAdapter(mBooks);
+        lw.setAdapter(mAdapter);
+        lw.setLayoutManager(new LinearLayoutManager(getContext()));
 
         // Inflate the layout for this fragment
         return rootView;
