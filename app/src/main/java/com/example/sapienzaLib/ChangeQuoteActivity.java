38529947cb7a5738d7e5b73a57654a488534b72f
LeoShelby <leo-dp-95@hotmail.it>
2019-03-08 20:00:43 +0100

    pop up on Change quote

--- app/src/main/java/com/example/sapienzaLib/ChangeQuoteActivity.java
+++ app/src/main/java/com/example/sapienzaLib/ChangeQuoteActivity.java
@@ -1,12 +1,21 @@
 package com.example.sapienzaLib;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -39,8 +48,35 @@ public class ChangeQuoteActivity extends AppCompatActivity {
 
                 TextView text = HomePageFragment.textQuote;
                 text.setText(q);
-                button.setEnabled(false);
-                button.setText("Changed!");
+                //button.setEnabled(false);
+                //button.setText("Changed!");
+
+
+                //Hide Keyboard
+                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_window, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when click OK
+                TextView ok = popupView.findViewById(R.id.ok_button);
+                ok.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
 
             }
         });
